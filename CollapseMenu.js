98cb970c697ec68bd5408b726dcec0064ad18670
Alex Curnow <alexcurnow@gmail.com>
2020-02-21 14:21:10 -0600

    experimented with button toggling the display property of a div

--- CollapseMenu.js
+++ CollapseMenu.js
@@ -1,23 +1,10 @@
-console.log('working!')
-
-const collapseTravelDetails = () => {
-  const details = document.querySelectorAll('.collapse')
-
-  const button = document.querySelectorAll('.details-btn')
-
-  for (const el of details) {
-    el.style.display = 'none'
-  }
-
-  for (const btn of button) {
-    btn.addEventListener('click', () => {
-      for (const el of details) {
-        el.style.display = 'block'
-    }
+const collapse = () => {
+  const button = document.querySelector('.details-btn')
+  
+  button.addEventListener('click', () => {
+    const e = document.querySelector('.collapse')
+    e.style.display == 'block' ? e.style.display = 'none' : e.style.display = 'block'
   })
 }
-}
 
-collapseTravelDetails()
-
-// export default collapseTravelDetails
+collapse()

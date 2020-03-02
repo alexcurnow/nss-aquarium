18dce582bf18ab76800ab1d3c62a2369b04bdd74
Alex Curnow <alexcurnow@gmail.com>
2020-03-02 17:37:38 -0600

    refactored fishlist component and fish data provider to organize fish by multiples of length

--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,14 +1,16 @@
-import { useFish, mostHolyFish, soldierFish, nonHolyFish } from './FishDataProvider.js'
+import {
+  mostHolyFish,
+  soldierFish,
+  nonHolyFish
+} from './FishDataProvider.js'
 import Fish from './Fish.js'
 
 const FishList = () => {
   let contentElement = document.querySelector('.fishList')
-  
+
   const fishes = mostHolyFish().concat(soldierFish(), nonHolyFish())
 
-    for (const fish of fishes) {
-     contentElement.innerHTML += Fish(fish)
-    }
+  fishes.forEach(fish => (contentElement.innerHTML += Fish(fish)))
 }
 
 export default FishList
